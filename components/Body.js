92547fcb622bfbf08d96da4ade80337f1e05299b
Aero-hacker <jagan.devarapu@gmail.com>
2024-01-08 15:19:37 +0530

    commit

---
+++ components/Body.js
@@ -0,0 +1,52 @@
+import Restaurantcard from "./RestaurantCard";
+import reslist from "../utils/Mockdata";
+import { useEffect, useState } from "react";
+
+
+
+const Body = () => {
+
+    const [listofrestaurants,setlistofrestaurants] = useState([])
+   
+    useEffect(() => {
+      fetchData();
+    },[]
+    )
+   const fetchData = async () => {
+      const data = await fetch("https://corsproxy.io/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83667")
+      const json = data.json();
+      console.log(json)
+      setlistofrestaurants(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants.info)
+   }
+
+    return(  
+    <div className='body'>
+          <div className='filter'>
+            <input className="input-box"/>
+            <button className="filter-btn" 
+             onClick={() => {
+              
+              const filteredlist = listofrestaurants.filter(
+                (res) => res.data.avgRating > 4
+              );
+              setlistofrestaurants(filteredlist)
+            }}>
+              Top Rated Restaurant
+            </button>
+            </div>
+          <div className='res-container'>
+            {listofrestaurants.map((restaurant) => 
+              <Restaurantcard
+               resData={restaurant}
+               key = {restaurant.data.id}
+              />)}
+             
+             
+             
+  
+          </div>
+      </div>
+    )
+  }
+  
+export default Body;
---
+++ utils/Mockdata.js
@@ -0,0 +1,149 @@
+const reslist = [{
+    type:"restaurant",
+    data:{
+        id:"0",
+        type:"F",
+        name:"KFC",
+        cuisines:["Burgers","Biryani","American","Fast food"],
+        avgrating:"4.5",
+        costfortwo:40000,
+        deliverytime:"35 minutes",
+        imgid:"w_660/195876a3181ef63f76e45e3a7b49b585"}
+    },
+    {
+        type:"restaurant",
+    data:{
+        id:"1",
+        type:"F",
+        name:"KFC",
+        cuisines:["Burgers","Biryani","American","Fast food"],
+        avgrating:"4.5",
+        costfortwo:40000,
+        deliverytime:"35 minutes",
+        imgid:"w_660/c9elzh8awx7jcx70cood"}
+    },
+    {
+    type:"restaurant",
+    data:{
+        id:"2",
+        type:"F",
+        name:"KFC",
+        cuisines:["Burgers","Biryani","American","Fast food"],
+        avgrating:"4.5",
+        costfortwo:40000,
+        deliverytime:"35 minutes",
+        imgid:",w_660/bb7ae131544c7d37e10fc5faf76f09d6"}
+    },
+    {
+        type:"restaurant",
+    data:{
+        id:"3",
+        type:"F",
+        name:"KFC",
+        cuisines:["Burgers","Biryani","American","Fast food"],
+        avgrating:"3.5",
+        costfortwo:40000,
+        deliverytime:"35 minutes",
+        imgid:"w_660/56c9ab92bd79745fd152a30fa2525426"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"4",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"3",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:"w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"5",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"3.5 ",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:",w_660/emlehbuwgsmryxhwzhvq"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"6",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"3.5",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:"w_660/6126c9b45de2cb222405c1af8a321e74"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"7",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"3.5 ",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:"w_660/aslqpj36wuh2gx4xfrrk"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"8",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"4.2 ",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:"w_660/bs9qgsy25pn07pzn6xx1"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"9",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"3.5 ",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:",w_208,h_208,c_fit/xdn3ovilifxvzoyexlaw"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"10",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"3.5 ",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:",w_660/r4mw2xp0djz0pscwmiop"}
+    },
+    {
+        type:"restaurant",
+        data:{
+            id:"11",
+            type:"F",
+            name:"KFC",
+            cuisines:["Burgers","Biryani","American","Fast food"],
+            avgrating:"3.5 ",
+            costfortwo:40000,
+            deliverytime:"35 minutes",
+            imgid:",w_660/hpp3mkis0pmjvnvqt8pl"}
+    }
+    
+  
+]
+
+export default reslist;
